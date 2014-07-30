07ed758e89b736a29ab3a17af29ee0cce2bd1fd1
nalabelle <nalabelle@gmail.com>
2014-07-30 11:15:55 -0700

    cleaned imports

--- src/com/comp380/towergame/physics/CollisionDetection.java
+++ src/com/comp380/towergame/physics/CollisionDetection.java
@@ -2,8 +2,6 @@ package com.comp380.towergame.physics;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 import java.util.Map.Entry;
 
 import android.graphics.Point;
